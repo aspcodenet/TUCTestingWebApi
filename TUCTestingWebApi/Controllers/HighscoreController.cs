58c35dc397ee2a6e95c14f7e4cb6008a2d12cd04
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-06-04 10:10:27 +0200

    Edit registation

--- TUCTestingWebApi/Controllers/HighscoreController.cs
+++ TUCTestingWebApi/Controllers/HighscoreController.cs
@@ -30,6 +30,40 @@ namespace TUCTestingWebApi.Controllers
             return View(viewModel);
         }
 
+        // Highscore/Edit/0    Highscore/Edit/1 
+        public IActionResult Edit(int id)
+        {
+            var viewModel = new HighscoreEditViewModel();
+
+            var entryFromDatabase = _highscoreEntryRepository.Get(id);
+            viewModel.Country = entryFromDatabase.Country;
+            viewModel.Game = entryFromDatabase.Game;
+            viewModel.Id = entryFromDatabase.Id;
+            viewModel.Namn = entryFromDatabase.Namn;
+            viewModel.Points = entryFromDatabase.Points;
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(HighscoreEditViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var entryFromDatabase = _highscoreEntryRepository.Get(viewModel.Id);
+                entryFromDatabase.Country = viewModel.Country;
+                entryFromDatabase.Game = viewModel.Game;
+                entryFromDatabase.Id = viewModel.Id;
+                entryFromDatabase.Namn = viewModel.Namn;
+                entryFromDatabase.Points = viewModel.Points;
+                return RedirectToAction("Index");
+            }
+
+            return View(viewModel);
+        }
+
+
+
 
         // /Highscore/New
 
@@ -38,6 +72,9 @@ namespace TUCTestingWebApi.Controllers
             var viewModel = new HighscoreNewViewModel();
             return View(viewModel);
         }
+
+
+
 
         [HttpPost]
         public IActionResult New(HighscoreNewViewModel viewModel)
--- TUCTestingWebApi/ViewModels/HighscoreNewViewModel.cs
+++ TUCTestingWebApi/ViewModels/HighscoreNewViewModel.cs
@@ -9,4 +9,16 @@
         public int Points { get; set; }
 
     }
+
+
+    public class HighscoreEditViewModel
+    {
+        public int Id { get; set; }
+        public string Game { get; set; }
+        public string Country { get; set; }
+        public string Namn { get; set; }
+        public int Points { get; set; }
+
+    }
+
 }
